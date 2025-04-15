d22d2940ee2a1ff74e58fa9d708482471a5b89f1
sumanth-sangireddy <sangireddysumanth@gmail.com>
2025-04-15 11:25:19 +0530

    Updated UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,6 @@ public class UserController {
 	
 	@GetMapping("/user")
 	 public String UserData() {
-		return "Hello World:Welcome to lenorasoft technologies private limited"; 
+		return "Hello World:Welcome to lenorasoft technologies private limited and BakertillyJFC"; 
 	 }
 }
